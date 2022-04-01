90087066d7e9ed6fe292eb108070239873b29e05
top14161416 <43666996+top14161416@users.noreply.github.com>
2022-04-01 17:26:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css"/>
+    <link rel="preconnect" href="http://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <section class="header">
+        <div class="nav-link">
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="./login.php" class="login-btn">Login</a></li>
+                </ul>
+           
+            </nav>
+        </div>
+        
+        <div class="text-box">
+        <h1>Design is the silent</h1>
+        <h1>ambassador of your</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ea quae sit eum repellat incidunt rerum enim, quis hic doloribus iure consectetur maxime nobis explicabo officiis odit magnam veniam! Labore?
+        </p>
+        <a href="" class="hero-btn">NEWS</a>
+        </div>
+        
+
+    </section>
+
+    <footer>
+        <div class="mockup">
+            <div class="mockup-box">
+                <p class="title-mockup">Mockup Company </p>
+                <p class="mockup-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolorem nostrum vitae ipsa cupiditate quis voluptatem provident error ipsum dignissimos?</p>
+            </div>
+            <div class="social">
+                <div class="social-container">
+                    <img src="./img/facebook.png" alt="" width="73" height="70">
+                </div>
+                <div class="social-container">
+                    <img src="./img/linkin.png" alt="" width="73" height="70">
+                </div>
+                <div class="social-container">
+                    <img src="./img/youtube.png" alt="" width="73" height="70">
+                </div>
+                <div class="social-container">
+                    <img src="./img/phone.png" alt="" width="73" height="70">
+                </div>
+            </div> 
+        </div>
+        
+        
+        <div class="table-box">
+                <table>
+                    <tr>
+                        <th>Products</th>
+                        <th>Services</th>
+                        <th>Resources</th>
+                    </tr>
+                    <tr>
+                        <td>Products</td>
+                        <td>Services</td>
+                        <td>Resources</td>
+                    </tr>
+                </table>
+            </div>
+    </footer>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="login-container">
+            <div class="login-form">
+                <h1 class="login-title">Sign in</h1>
+                <p class="creaccount">new user?<a href="">Create an account</a></p>
+                <br>
+                <input type="text" placeholder="username" name="username" required="">
+                <br>
+                <br>
+                <input type="password" placeholder="password" name="password" required="">
+                <br>
+                <br>
+                <a href="" class="hero-btn">Singin</a>
+                <br>
+                <br>
+                <a class="forgetpass" href="#">forget password ?</a>
+            </div>
+            <div class="img-container">
+                <img src="./img/pic1.png" alt="" width="340" height="450">
+            </div>
+            <div class="img-home">
+                <img src="./img/home.png" alt="" width="100" height="100">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
